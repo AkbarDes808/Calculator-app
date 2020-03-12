17018f4a054d95e84b598ee36eb37937a07a3900
akbar <akbar.26@students.amikom.ac.id>
2020-03-12 23:12:27 +0700

    menambahkan aplikasi kalkulator

---
+++ Tugas1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkulator sederhana");
+            Console.WriteLine("1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+            Console.WriteLine("");
+            Console.WriteLine("Pilih menu perhitungan(1,2,3,4) :");
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Masukkan angka pertama:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Masukkan Angka kedua:");
+                int c = Convert.ToInt32(Console.ReadLine());
+                {
+                    if (a == 1)
+                    {
+                        int penjumlahan = b + c;
+                        Console.WriteLine($"Maka hasil penjumlahan = {penjumlahan}");
+                    }
+                    else if (a == 2)
+                    {
+                        int pengurangan = b - c;
+                        Console.WriteLine($"Maka Hasil Pengurangan = {pengurangan}");
+                    }
+                    else if (a == 3)
+                    {
+                        int perkalian = b * c;
+                        Console.WriteLine($"Maka Hasil Perkalian = {perkalian}");
+                    }
+                    else if (a == 4)
+                    {
+                        int pembagian = b / c;
+                        Console.WriteLine($"Maka Hasil Pembagian = {pembagian}");
+                    }
+                }
+            }
+        }
+    }
+}
